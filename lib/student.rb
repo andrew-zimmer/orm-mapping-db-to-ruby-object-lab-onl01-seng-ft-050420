92af72da1d7f8537586e246957c4f15941fd7fe8
Andrew Zimmer <andrewzimmer.dev@gmail.com>
2020-06-03 17:00:04 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -18,8 +18,7 @@ class Student
     sql = "SELECT * FROM students WHERE name = ? LIMIT 1;"
     DB[:conn].execute(sql, name).collect do |row| 
       self.new_from_db(row)
-    end.first
-    
+    end
   end
   
   def save
