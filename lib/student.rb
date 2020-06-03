5dddd1e21214d5e969e4109d694e86cc593279d8
Andrew Zimmer <andrewzimmer.dev@gmail.com>
2020-06-03 17:31:41 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -57,7 +57,7 @@ class Student
   
   def self.all_students_in_grade_X(x)
     sql = <<-SQL 
-      "SELECT * FROM students 
+      SELECT * FROM students 
       WHERE grade = ? 
     SQL
     DB[:conn].execute(sql, x)
