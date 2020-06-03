8342c5caa702ee6c23853be587a6a4fa15fa863a
Andrew Zimmer <andrewzimmer.dev@gmail.com>
2020-06-03 17:21:42 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -43,7 +43,7 @@ class Student
       WHERE grade = 10 
       LIMIT ?;
     SQL
-    DB[:conn].execute(sql, x)
+    DB[:conn].execute(sql, x).collect {|row| self.new_from_db(row)}
   end 
 
   def self.find_by_name(name)
