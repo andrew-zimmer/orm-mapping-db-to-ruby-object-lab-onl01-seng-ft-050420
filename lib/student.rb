517d632df4dc0ff3d132d002f224dee092f78855
Andrew Zimmer <andrewzimmer.dev@gmail.com>
2020-06-03 17:03:03 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,6 +13,13 @@ class Student
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
   end
+  
+  def self.all_students_in_grade_9
+    sql = <<-SQL 
+      SELECT * FROM students 
+      WHERE grade = 9
+    SQL
+  end 
 
   def self.find_by_name(name)
     sql = "SELECT * FROM students WHERE name = ? LIMIT 1;"
